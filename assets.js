0d45f1c080771143fc2f5fb5c84aa2c8f903f041
fifa3827-lgtm <fifa3827@gmail.com>
2026-09-23 02:03:13 +0900

    feat: 에셋 경로를 한 파일로 모음
    유리 텍스처 7장과 소리 4개의 경로를 assets.js 한 곳에 모았다.
    
    에셋을 갈아끼울 때 게임 코드를 건드리지 않아도 되도록 분리했다.

---
+++ assets.js
@@ -0,0 +1,10 @@
+/* 에셋 경로 */
+const TEX = {
+  R:'assets/glass_R.png', Y:'assets/glass_Y.png', B:'assets/glass_B.png',
+  O:'assets/glass_O.png', G:'assets/glass_G.png', P:'assets/glass_P.png',
+  N:'assets/glass_N.png'
+};
+const AUD = {
+  bgm:'assets/bgm.mp3', win:'assets/win.mp3',
+  drop:'assets/drop.mp3', mix:'assets/mix.mp3'
+};
